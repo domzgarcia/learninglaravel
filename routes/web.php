fcc57a9de2770096df02f4d07500c87ff1ccfc57
bb_domgarcia <dom.garcia@nuworks.ph>
2018-10-25 22:26:31 +0800

    debugging

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/home', 'HomeController@index')
+       ->name('home');
+       
